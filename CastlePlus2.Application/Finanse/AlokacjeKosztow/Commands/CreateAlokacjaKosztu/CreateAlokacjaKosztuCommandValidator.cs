ac28e9864136bfc4c13c863d502d0fd0592d498a
Michal <m.p.garnuch@gmail.com>
2025-12-28 13:53:07 +0100

    Alokacja Kosztu - dodano validacje

---
+++ CastlePlus2.Application/Finanse/AlokacjeKosztow/Commands/CreateAlokacjaKosztu/CreateAlokacjaKosztuCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace CastlePlus2.Application.Finanse.AlokacjeKosztow.Commands.CreateAlokacjaKosztu
+{
+    public sealed class CreateAlokacjaKosztuCommandValidator : AbstractValidator<CreateAlokacjaKosztuCommand>
+    {
+        public CreateAlokacjaKosztuCommandValidator()
+        {
+            // Reguła z SQL: IdPozycjiKosztu bigint NOT NULL
+            RuleFor(x => x.IdPozycjiKosztu)
+                .NotEmpty();
+
+            // Reguła z SQL: IdEncji uniqueidentifier NOT NULL
+            RuleFor(x => x.IdEncji)
+                .NotEmpty();
+
+            // Reguła z SQL: KwotaNetto decimal(18,2) NOT NULL
+            RuleFor(x => x.KwotaNetto)
+                .NotEmpty();
+
+            // Reguła z SQL: KwotaBrutto decimal(18,2) NOT NULL
+            RuleFor(x => x.KwotaBrutto)
+                .NotEmpty();
+        }
+    }
+}
---
+++ CastlePlus2.Application/Finanse/AlokacjeKosztow/Commands/UpdateAlokacjaKosztu/UpdateAlokacjaKosztuCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace CastlePlus2.Application.Finanse.AlokacjeKosztow.Commands.UpdateAlokacjaKosztu
+{
+    public sealed class UpdateAlokacjaKosztuCommandValidator : AbstractValidator<UpdateAlokacjaKosztuCommand>
+    {
+        public UpdateAlokacjaKosztuCommandValidator()
+        {
+            // Reguła z SQL: IdAlokacji bigint NOT NULL
+            RuleFor(x => x.IdAlokacji)
+                .NotEmpty();
+
+            // Reguła z SQL: IdPozycjiKosztu bigint NOT NULL
+            RuleFor(x => x.IdPozycjiKosztu)
+                .NotEmpty();
+
+            // Reguła z SQL: IdEncji uniqueidentifier NOT NULL
+            RuleFor(x => x.IdEncji)
+                .NotEmpty();
+
+            // Reguła z SQL: KwotaNetto decimal(18,2) NOT NULL
+            RuleFor(x => x.KwotaNetto)
+                .NotEmpty();
+
+            // Reguła z SQL: KwotaBrutto decimal(18,2) NOT NULL
+            RuleFor(x => x.KwotaBrutto)
+                .NotEmpty();
+        }
+    }
+}
